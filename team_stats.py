9b213bd27708d42d65b7372b4d7c3b575e7ee5e4
tranthao4 <thao_25@hotmail.com>
2022-08-30 16:33:13 +0200

    uuid and results per team

--- team_stats.py
+++ team_stats.py
@@ -2,23 +2,23 @@ import requests
 
 base_url = 'https://www.shl.se/'
 shl_standing_path = 'p/api/statistics/standings_standings?ssgtUuid=qZl-8qb98ZuHk&count=25'
-rbk_path = 'p/api/statistics/games_games?ssgtUuid=qZl-8qb98ZuHk&teamUuid=ee93-ee93uy4oW&count=25'
+games_path = 'p/api/statistics/games_games?ssgtUuid=qZl-8qb98ZuHk&teamUuid='
 
 teams = [
-    {'TeamCode': 'RBK', 'TeamName': 'Rögle BK'},
-    {'TeamCode': 'LHF', 'TeamName': 'Luleå Hockey'},
-    {'TeamCode': 'SAIK', 'TeamName': 'Skellefteå AIK'},
-    {'TeamCode': 'FHC', 'TeamName': 'Frölunda HC'},
-    {'TeamCode': 'VLH', 'TeamName': 'Växjö Lakers'},
-    {'TeamCode': 'FBK', 'TeamName': 'Färjestad BK'},
-    {'TeamCode': 'OHK', 'TeamName': 'Örebro Hockey'},
-    {'TeamCode': 'LIF', 'TeamName': 'Leksands IF'},
-    {'TeamCode': 'IKO', 'TeamName': 'IK Oskarshamn'},
-    {'TeamCode': 'BIF', 'TeamName': 'Brynäs IF'},
-    {'TeamCode': 'LHC', 'TeamName': 'Linköping HC'},
-    {'TeamCode': 'MIF', 'TeamName': 'Malmö Redhawks'},
-    {'TeamCode': 'DIF', 'TeamName': 'Djurgården Hockey'},
-    {'TeamCode': 'TIK', 'TeamName': 'Timrå IK'},
+    {'TeamCode': 'RBK', 'TeamName': 'Rögle BK', 'uuid': 'ee93-ee93uy4oW'},
+    {'TeamCode': 'LHF', 'TeamName': 'Luleå Hockey', 'uuid': '1a71-1a71gTHKh'},
+    {'TeamCode': 'SAIK', 'TeamName': 'Skellefteå AIK', 'uuid': '50e6-50e6DYeWM'},
+    {'TeamCode': 'FHC', 'TeamName': 'Frölunda HC', 'uuid': '087a-087aTQv9u'},
+    {'TeamCode': 'VLH', 'TeamName': 'Växjö Lakers', 'uuid': 'fe02-fe02mf1FN'},
+    {'TeamCode': 'FBK', 'TeamName': 'Färjestad BK', 'uuid': '752c-752c12zB7Z'},
+    {'TeamCode': 'OHK', 'TeamName': 'Örebro Hockey', 'uuid': '82eb-82ebmgaJ8'},
+    {'TeamCode': 'LIF', 'TeamName': 'Leksands IF', 'uuid': '9541-95418PpkP'},
+    {'TeamCode': 'IKO', 'TeamName': 'IK Oskarshamn', 'uuid': '259b-259bYGVIp'},
+    {'TeamCode': 'BIF', 'TeamName': 'Brynäs IF', 'uuid': '1ab8-1ab8bfj7N'},
+    {'TeamCode': 'LHC', 'TeamName': 'Linköping HC', 'uuid': '41c4-41c4BiYZU'},
+    {'TeamCode': 'MIF', 'TeamName': 'Malmö Redhawks', 'uuid': '8e6f-8e6fUXJvi'},
+    {'TeamCode': 'DIF', 'TeamName': 'Djurgården Hockey', 'uuid': '2459-2459QTs1f'},
+    {'TeamCode': 'TIK', 'TeamName': 'Timrå IK', 'uuid': '31d1-31d1NbSlR'},
 ]
 
 class Team:
@@ -37,10 +37,19 @@ class Team:
                 return team
         return None
 
-    def get_team_results():
-        json = requests.get(base_url + rbk_path).json()
-        for items in json:
-            for x in items:
-                print(x)
-            print(items['stats'])
+    def get_team_results(team_code):
+        json = requests.get(f'{base_url}{games_path}{Team._get_team_uuid(team_code)}&count=52').json()[0]
+        all_games = json['games']
+        for game_id in all_games:
+            print(all_games[game_id]['homeTeamName'])
+            print(all_games[game_id]['homeTeamResult'])
+            print(all_games[game_id]['awayTeamName'])
+            print(all_games[game_id]['awayTeamResult'])
+            print(all_games[game_id]['periodResults'])
+            print(all_games[game_id]['overtime'])
+            break
                 
+    def _get_team_uuid(team_code):
+        for team in teams:
+            if team_code == team['TeamCode']:
+                return team['uuid']
